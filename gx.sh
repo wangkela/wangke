4e6ad2547861a9f326fdcebe9bbfa55751d4a3f8
wangkela <zhongcanchen6@gmail.com>
2026-03-25 22:34:52 +0800

    Add files via upload

--- gx.sh
+++ gx.sh
@@ -4,7 +4,7 @@ DOWNLOAD_URL="https://github.com/wangkela/wangke/archive/refs/heads/main.zip"
 ZIP_FILE="main.zip"
 EXTRACT_DIR="wangke-main"
 MODULE_DIR="/data/adb/modules/wangke"
-TOTAL_SIZE=247413  # 总文件大小
+TOTAL_SIZE=247418  # 总文件大小
 
 # 清理旧的模块文件和文件夹
 rm -rf $MODULE_DIR/$ZIP_FILE
